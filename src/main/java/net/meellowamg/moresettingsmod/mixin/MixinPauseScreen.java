44397edba1e5965f60a98e42bb01ebbf4fba77d6
meellowamg-lang <meellowamg@gmail.com>
2026-07-21 03:47:14 +0200

    Small Change

--- src/main/java/net/meellowamg/moresettingsmod/mixin/MixinPauseScreen.java
+++ src/main/java/net/meellowamg/moresettingsmod/mixin/MixinPauseScreen.java
@@ -3,10 +3,8 @@ package net.meellowamg.moresettingsmod.mixin;
 import net.meellowamg.moresettingsmod.MoreSettingsScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Renderable;
-import net.minecraft.client.gui.components.events.GuiEventListener;
-import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,22 +12,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PauseScreen.class)
+@Mixin(Screen.class)
 public class MixinPauseScreen {
 
-    @Shadow protected Minecraft minecraft;
     @Shadow protected int width;
     @Shadow protected int height;
 
-    @Shadow
-    protected <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(T widget) {
-        throw new AssertionError();
-    }
+    @Inject(method = "init(Lnet/minecraft/client/Minecraft;II)V", at = @At("TAIL"))
+    private void onInit(Minecraft minecraft, int width, int height, CallbackInfo ci) {
+        // Only add button on the pause screen
+        if (!((Object)this instanceof PauseScreen)) return;
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void onInit(CallbackInfo ci) {
-        PauseScreen self = (PauseScreen)(Object)this;
-        addRenderableWidget(Button.builder(
+        Screen self = (Screen)(Object)this;
+
+        self.addRenderableWidget(Button.builder(
                 Component.literal("Full Settings"),
                 btn -> minecraft.gui.setScreen(new MoreSettingsScreen(self))
         ).bounds(width / 2 - 102, height / 4 + 155, 204, 20).build());
