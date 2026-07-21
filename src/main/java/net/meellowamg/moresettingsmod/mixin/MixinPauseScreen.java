550ac46574d108afe7f21f51ea5c66944d306fbb
meellowamg-lang <meellowamg@gmail.com>
2026-07-21 13:08:49 +0200

    Small Change

--- src/main/java/net/meellowamg/moresettingsmod/mixin/MixinPauseScreen.java
+++ src/main/java/net/meellowamg/moresettingsmod/mixin/MixinPauseScreen.java
@@ -3,6 +3,9 @@ package net.meellowamg.moresettingsmod.mixin;
 import net.meellowamg.moresettingsmod.MoreSettingsScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -18,16 +21,18 @@ public class MixinPauseScreen {
     @Shadow protected int width;
     @Shadow protected int height;
 
+    @Shadow
+    protected <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(T widget) {
+        throw new AssertionError();
+    }
+
     @Inject(method = "init(Lnet/minecraft/client/Minecraft;II)V", at = @At("TAIL"))
     private void onInit(Minecraft minecraft, int width, int height, CallbackInfo ci) {
-        // Only add button on the pause screen
         if (!((Object)this instanceof PauseScreen)) return;
 
-        Screen self = (Screen)(Object)this;
-
-        self.addRenderableWidget(Button.builder(
+        addRenderableWidget(Button.builder(
                 Component.literal("Full Settings"),
-                btn -> minecraft.gui.setScreen(new MoreSettingsScreen(self))
+                btn -> minecraft.gui.setScreen(new MoreSettingsScreen((Screen)(Object)this))
         ).bounds(width / 2 - 102, height / 4 + 155, 204, 20).build());
     }
 }
